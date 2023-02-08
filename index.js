54647b4feb34aea2a02c641e1fef2aee39a2158c
mymythboy11ggo <mymythboy11ggo@gmail.com>
2023-02-08 16:45:00 +0700

    Morgan

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
 // route
+app.use(morgan('combined'))
+
 app.get("/tin-tuc", (req, res) => {
-  var a = 1;
-  var b = 2;
-  var c = a + b;
   res.send("Hello World!");
 });
 
